2680fdb56d3b23ee191c306567744340501a5e42
Steven Pautz <spautz@gmail.com>
2022-08-04 21:47:19 -0400

    Migrate to tsup and fix code coverage reports

---
+++ vitest.config.ts
@@ -0,0 +1,16 @@
+import {configDefaults,defineConfig} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    deps: {
+      inline: [/dynamic-selectors/],
+    },
+    coverage: {
+      exclude: [
+        ...configDefaults.exclude,
+        '**/__tests__/**',
+        '**/legacy-types/**'
+      ]
+    },
+  },
+});
